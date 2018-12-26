fa8afe5e0e4907b9afa0b4f3edd2cb9de0ffb04a
satoumtzk <haniyooo0723@gmail.com>
2018-12-26 21:22:14 +0900

    20181226

--- Person2/src/jp/co/internous/action/Test.java
+++ Person2/src/jp/co/internous/action/Test.java
@@ -8,9 +8,24 @@ public class Test {
 		System.out.println(taro.name);
 		System.out.println(taro.age);
 
+        Person jiro=new Person("jiro",20);
+        System.out.println(jiro.name);
+        System.out.println(jiro.age);
 
+        Person saburo=new Person("saburo");
+		saburo.name="saburo";
+		System.out.println(saburo.name);
+		System.out.println(saburo.age);
 
+		Person nanasi=new Person(25);
+		nanasi.age=25;
+		System.out.println(nanasi.name);
+		System.out.println(nanasi.age);
 
+		Person hanako=new Person(17,"hanako");
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
 	}
+
 
 }
